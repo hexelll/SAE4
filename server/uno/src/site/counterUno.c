3378d82de84a379c30db7b9bfe67f356ff99161e
Luc Casassus Latrille <l.casassus-latrille@etud.univ-pau.fr>
2026-04-09 18:58:16 +0200

    started working on counterUno, fixed segfault in counterUno

---
+++ server/uno/src/site/counterUno.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include "/myserver/libs/utils/database.c"
+#include "/myserver/libs/server.c"
+#include "/myserver/libs/utils/list.c"
+
+List getCardsForPlayer(String playerid,struct Arena* arena, Connection theconnection) {
+    QueryResult resnb = ConnectionSelect(theconnection, StringFormatChar(arena,"select * from gamecard,usercard,player where gamecard.cardid = usercard.cardid and usercard.playerid = player.playerid and player.playerid = %S",playerid));
+    return QueryResultToList(resnb,arena);
+}
+
+int getPlayerGameId(String playerid, struct Arena* arena, Connection theconnection){
+    QueryResult resPlayers = ConnectionSelect(theconnection, StringFormatChar(arena, "select joinedgameid from player where playerid =%S", playerid));
+    List tuples = QueryResultToList(resPlayers,arena);
+    Hashmap* user = ListGetVal(&tuples,0)->ptr;
+    String* gameId = HashmapGet(user,"joinedgameid");
+    int gameIdNumber = -1;
+    StringToInt(*gameId , &gameIdNumber);
+    return gameIdNumber;
+}
+
+String makeResponse(struct Arena* arena,Hashmap map) {
+    Connection con = ConnectionNew("unodb","uno","root","pass4root","5432");
+
+    String err = StringFrom("",arena);
+    String* userId = HashmapGet(&map,"userId");
+    int userIdnb = 0;
+    err = userId ? err : StringConcat(err,StringFrom("missing userId in request ",arena),arena);
+    userIdnb = userId ? StringToInt(*userId, &userIdnb) : -1;
+
+    String* userPwd = HashmapGet(&map,"userPwd");
+    err = userPwd ? err : StringConcat(err,StringFrom("missing userPwd in request ",arena),arena);
+
+    if(!userId || !userPwd) {
+        return StringFormatChar(arena,"{\"ok\":false,\"error\":\"%S\"}",err);
+    }
+
+    int gameId = getPlayerGameId(*userId, arena, con);
+    if(gameId <= 0){
+        return StringFormatChar(arena,"{\"ok\":false,\"error\":\"could not find game\"}");
+    }
+    
+
+    QueryResult res = ConnectionSelect(con,StringFormatChar(arena,"select * from player where playerid = %d and userpwd = '%S'",userIdnb,*userPwd));
+    if(!(res.count > 0 && res.message.size == 0)) {
+        return StringFormatChar(arena,"{\"ok\":false,\"error\":\"no user with this id and password\"}");
+    }
+
+    List nbCards = getCardsForPlayer(*userId, arena, con);
+    if(!(nbCards.size > 0)){
+        return StringFormatChar(arena,"{\"ok\":false,\"error\":\"couldn't retrieve number of card in the player's current hand\"}");
+    }
+
+    if(nbCards.size > 1){
+        return StringFormatChar(arena,"{\"ok\":false,\"error\":\"player does not have 1 card left in hand\"}");
+    }
+
+    ConnectionClose(con);
+    return StringFormatChar(arena,"{\"ok\":true}");
+}
+
+int main(int argc,char** argv) {
+    ServerInitDefault();
+    struct Arena sarena = ArenaCreate(1024);
+
+    struct Arena* arena = &sarena;
+
+    String response = makeResponse(arena,ServerParseRequest(argv,arena));
+
+    ServerRespond(200,StringFrom("{\"Content-Type\":\"application/json\"}",arena),response,arena);
+    ArenaDelete(arena);
+}

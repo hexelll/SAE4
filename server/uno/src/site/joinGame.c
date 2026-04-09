e5c8e44913ddd1dc8d0262821513c5739ab54480
hexelll <139350367+hexelll@users.noreply.github.com>
2026-04-09 10:53:34 +0200

    made joinGame nicer to look at

--- server/uno/src/site/joinGame.c
+++ server/uno/src/site/joinGame.c
@@ -2,17 +2,9 @@
 #include "/myserver/libs/utils/database.c"
 #include "/myserver/libs/server.c"
 
-int main(int argc,char** argv) {
-    ServerInitDefault();
-    struct Arena sarena = ArenaCreate(1024);
-
-    struct Arena* arena = &sarena;
-
-    String response;
-
+String makeResponse(struct Arena* arena,Hashmap map) {
     Connection con = ConnectionNew("unodb","uno","root","pass4root","5432");
     
-    Hashmap map = ServerParseRequest(argv,arena);
     String err = StringFrom("",arena);
     String* userId = HashmapGet(&map,"userId");
     err = userId ? err : StringConcat(err,StringFrom("missing userId in request ",arena),arena);
@@ -20,27 +12,40 @@ int main(int argc,char** argv) {
     err = userPwd ? err : StringConcat(err,StringFrom("missing userPwd in request ",arena),arena);
     String* gameCode = HashmapGet(&map,"gameCode");
     err = gameCode ? err : StringConcat(err,StringFrom("missing gameCode in request ",arena),arena);
-
     if(!userId || !userPwd || !gameCode) {
-        response = StringFormatChar(arena,"{\"ok\":false,\"error\":\"%S\"}",err);
-    }else {
-        QueryResult res = ConnectionSelect(con,StringFormatChar(arena,"select * from game where code = '%S'",*gameCode));
-        if (res.count > 0 && res.message.size == 0) {
-            List tuples = QueryResultToList(res,arena);
-            String* gameId = (String*)HashmapGet(ListGetVal(&tuples,0)->ptr,"gameid");
-            res = ConnectionSelect(con,StringFormatChar(arena,"select * from player where playerid = %S and userpwd = '%S'",*userId,*userPwd));
-            if (res.count > 0 && res.message.size == 0) {
-                response = StringFormatChar(arena,"{\"ok\":true}");
-                res = ConnectionExec(con,StringFormatChar(arena,"update player set joinedgameid = %S where playerid = %S",*gameId,*userId));
-            }else {
-                response = StringFormatChar(arena,"{\"ok\":false,\"error\":\"no user with this id and password\"}");
-            }
-        }else {
-            response = StringFormatChar(arena,"{\"ok\":false,\"error\":\"no game with this code\"}");
-        }
+        return StringFormatChar(arena,"{\"ok\":false,\"error\":\"%S\"}",err);
     }
 
-    ServerRespond(200,StringFrom("{\"Content-Type\":\"application/json\"}",arena),response,arena);
+    QueryResult res = ConnectionSelect(con,StringFormatChar(arena,"select * from game where code = '%S'",*gameCode));
+    if (!(res.count > 0 && res.message.size == 0)) {
+        return StringFormatChar(arena,"{\"ok\":false,\"error\":\"no game with this code\"}");
+    }
+
+    List tuples = QueryResultToList(res,arena);
+    String* gameId = (String*)HashmapGet(ListGetVal(&tuples,0)->ptr,"gameid");
+
+    res = ConnectionSelect(con,StringFormatChar(arena,"select * from player where playerid = %S and userpwd = '%S'",*userId,*userPwd));
+    if(!(res.count > 0 && res.message.size == 0)) {
+        return StringFormatChar(arena,"{\"ok\":false,\"error\":\"no user with this id and password\"}");
+    }
+    
+    res = ConnectionExec(con,StringFormatChar(arena,"update player set joinedgameid = %S where playerid = %S",*gameId,*userId));
+    if(res.message.size != 0) {
+        return StringFormatChar(arena,"{\"ok\":false,\"error\":\"an error occured in insertion\"}");
+    }
+
     ConnectionClose(con);
+    return StringFormatChar(arena,"{\"ok\":true}");
+}
+
+int main(int argc,char** argv) {
+    ServerInitDefault();
+    struct Arena sarena = ArenaCreate(1024);
+
+    struct Arena* arena = &sarena;
+
+    String response = makeResponse(arena,ServerParseRequest(argv,arena));
+
+    ServerRespond(200,StringFrom("{\"Content-Type\":\"application/json\"}",arena),response,arena);
     ArenaDelete(arena);
 }

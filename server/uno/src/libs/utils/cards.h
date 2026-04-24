9805b04a2b1154bdab4209f7ee48bb5c832fc14e
hexelll <139350367+hexelll@users.noreply.github.com>
2026-04-24 13:26:21 +0200

    fixed incorrect function definitions

--- server/uno/src/libs/utils/cards.h
+++ server/uno/src/libs/utils/cards.h
@@ -12,14 +12,28 @@ typedef struct {
 
 Card CardFindById(int id,Connection con);
 
-List CardListForPlayer(int id,Connection con);
+Card CardFromMap(Hashmap cardmap,Connection con);
 
-List CardListForPlayedPile(int id,Connection con);
+List CardGetListForPlayer(int id,Connection con);
 
-List CardListForDrawPile(int id,Connection con);
+List CardGetListForPlayedPile(int id,Connection con);
+
+List CardGetListForDrawPile(int id,Connection con);
 
 QueryResult CardInsert(Card* card,Connection con);
 
 QueryResult CardDelete(int id,Connection con);
 
-Card CardFromMap(Hashmap cardmap,Connection con);
+QueryResult CardAddToPlayer(int cardid,int playerid,Connection con);
+
+QueryResult CardRemoveFromPlayer(int cardid,int playerid,Connection con);
+
+QueryResult CardAddToPlayed(int cardid,int gameid,Connection con);
+
+QueryResult CardRemoveFromPlayed(int cardid,int gameid,Connection con);
+
+QueryResult CardAddToDraw(int cardid,int gameid,Connection con);
+
+QueryResult CardRemoveFromDraw(int cardid,int gameid,Connection con);
+
+QueryResult CardAddToDeck(int cardid,int deckid,Connection con);
